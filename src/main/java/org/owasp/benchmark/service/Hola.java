0815149ac34638266b973fbb4a333524beec6407
andres quevedo trust it <andres.quevedo@sqasa.co>
2024-08-30 01:26:18 +0000

    Se agregó otro Print

--- src/main/java/org/owasp/benchmark/service/Hola.java
+++ src/main/java/org/owasp/benchmark/service/Hola.java
@@ -3,5 +3,6 @@ package org.owasp.benchmark.service;
 public class Hola {
     public static void main(String[] args) {
         System.out.println("Saludo");
+        System.out.println("Otro");
     }
 }
